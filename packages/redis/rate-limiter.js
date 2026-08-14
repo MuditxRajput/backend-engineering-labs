9823a0034b32f718eb5173ed5aa8a20a3aa5f40d
MuditxRajput <Muditrajputpersonal@gmail.com>
2026-08-15 00:58:13 +0530

    add rate limit code using sliding window algo but it does not prevent race condition

---
+++ packages/redis/rate-limiter.js
@@ -0,0 +1,27 @@
+// for rate limiter i use the sliding window algo...
+import connection from "./redis.connection.js";
+import crypto from 'crypto'
+const KEY="resend:sliding";
+const WINDOW = 60*1000;
+const LIMIT = 100;
+export const allowedResend = async()=>{
+    try {
+       const now = Date.now();
+       await connection.zremrangebyscore(KEY,0,now-WINDOW);
+
+       const count = await connection.zcard(KEY);
+       if(count>=LIMIT)
+       {
+        return false;
+       }
+
+       await connection.zadd(KEY,{
+        score : now,
+        value: crypto.randomUUID()
+       });
+       return true;
+    } catch (error) {
+        console.log("Rate limited error:",error);
+        return false;
+    }
+}
