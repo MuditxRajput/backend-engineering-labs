44a7f569c2b16b062de8b8a9fc4faf1fa0586905
MuditxRajput <Muditrajputpersonal@gmail.com>
2026-08-24 01:27:06 +0530

    add rate limit in notification worker

--- packages/redis/rate-limiter.js
+++ packages/redis/rate-limiter.js
@@ -1,11 +1,12 @@
 // for rate limiter i use the sliding window algo...
 import connection from "./redis.connection.js";
 import crypto from 'crypto'
-const KEY="resend:sliding";
-const WINDOW = 60*1000;
-const LIMIT = 100;
+
 export const allowedResend = async()=>{
     try {
+        const KEY="resend:sliding";
+        const WINDOW = 60*1000;
+        const LIMIT = 100;
        const now = Date.now();
        await connection.zremrangebyscore(KEY,0,now-WINDOW);
 
@@ -24,3 +25,4 @@ export const allowedResend = async()=>{
         return false;
     }
 }
+
--- packages/redis/redis.connection.js
+++ packages/redis/redis.connection.js
@@ -1,7 +1,8 @@
 import Redis from 'ioredis'
 const connection = new Redis({
     host : "localhost",
-    port : 6379,
+    // port : 8666,
+    port: 6379,
     maxRetriesPerRequest : null,
 })
 export default connection;
